4680d5d484f23728b9c1d80b0968ced72feda875
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-10-25 22:38:34 -0500

    Actualización 4.7

--- app.js
+++ app.js
@@ -62,3 +62,32 @@ function aboutThree(){
     aboutTargetTwo.classList.remove('active')
     aboutTargetThree.classList.add('active')
 }
+
+//Contact
+// const modalThanks = document.querySelector('.thanksContainer')
+// function thanksActive(){
+//     thanks.classList.add('thanksContainer__active')
+// }
+
+// closeThanks = document.querySelector('.closeThanks')
+// closeThanks.addEventListener('click', function(){
+//     thanks.classList.remove('thanksContainer__active')
+// })
+
+// const $form = document.querySelector('#form')
+// $form.addEventListener('submit', handleSubmit)
+// async function handleSubmit(event){
+//     event.preventDefault()
+//     const form = new FormData(this)
+//     const response = await fetch(this.action, {
+//         method: this.method,
+//         body: form,
+//         headers:{
+//             'Accept': 'aplication/json'
+//         }
+//     })
+//     if (response.ok){
+//         this.reset()
+//         thanksActive()
+//     }
+// }
