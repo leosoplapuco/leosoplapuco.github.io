81dfb07909936bf0ba8e5f750ef9dfa87a4e6edf
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2024-01-16 21:55:58 -0500

    Actualización 6.1

--- app.js
+++ app.js
@@ -1,150 +1,102 @@
-//AppData
-const github = 'https://github.com/leosoplapuco';
-const instagram = 'https://www.instagram.com/leosoplapuco/';
-const linkedIn = 'https://www.linkedin.com/in/leosoplapuco/';
+//Schools
+const schools = document.querySelector('.schools');
 
-const GitHub = document.querySelectorAll(".github");
-for (const githubLinks of GitHub) {
-    githubLinks.href = github;
-    githubLinks.innerHTML = 'GitHub';
-};
+const schoolButtonOne = document.querySelector('.schools-button_1');
+schoolButtonOne.addEventListener('click', function(){
+    schools.style.marginLeft = '0';
 
-const Instagram = document.querySelectorAll(".instagram");
-for (const instagramLink of Instagram) {
-    instagramLink.href = instagram;
-    instagramLink.innerHTML = 'Instagram';
-};
+    schoolButtonOne.classList.add('active');
+    schoolButtonTwo.classList.remove('active');
+    schoolButtonThree.classList.remove('active');
+    schoolButtonFour.classList.remove('active');
+});
 
-const LinkedIn = document.querySelectorAll(".linkedIn");
-for (const linkedInLink of LinkedIn) {
-    linkedInLink.href = linkedIn;
-    linkedInLink.innerHTML = 'LinkedIn';
-};
+const schoolButtonTwo = document.querySelector('.schools-button_2');
+schoolButtonTwo.addEventListener('click', function(){
+    schools.style.marginLeft = '-100%';
+
+    schoolButtonOne.classList.remove('active');
+    schoolButtonTwo.classList.add('active');
+    schoolButtonThree.classList.remove('active');
+    schoolButtonFour.classList.remove('active');
+});
+
+const schoolButtonThree = document.querySelector('.schools-button_3');
+schoolButtonThree.addEventListener('click', function(){
+    schools.style.marginLeft = '-200%';
+
+    schoolButtonOne.classList.remove('active');
+    schoolButtonTwo.classList.remove('active');
+    schoolButtonThree.classList.add('active');
+    schoolButtonFour.classList.remove('active');
+});
+
+const schoolButtonFour = document.querySelector('.schools-button_4');
+schoolButtonFour.addEventListener('click', function(){
+    schools.style.marginLeft = '-300%';
+
+    schoolButtonOne.classList.remove('active');
+    schoolButtonTwo.classList.remove('active');
+    schoolButtonThree.classList.remove('active');
+    schoolButtonFour.classList.add('active');
+});
 
 //Proyects
-// const proyectOne = document.querySelector('.proyect-1');
-// proyectOne.addEventListener('click', proyectOneActive);
+const proyectOne = document.querySelector('.proyect-1')
+const proyectButtonOne = document.querySelector('.proyect-button_1');
+proyectButtonOne.addEventListener('click', function(){
+    proyectOne.classList.add('active');
+    proyectTwo.classList.remove('active');
+    proyectThree.classList.remove('active');
+    proyectFour.classList.remove('active');
+    proyectFive.classList.remove('active');
+})
 
-// function proyectOneActive(){
-//     proyectOne.classList.add('active');
-//     proyectTwo.classList.remove('active');
-//     proyectThree.classList.remove('active');
-//     proyectFour.classList.remove('active');
-//     proyectFive.classList.remove('active');
-//     proyectSix.classList.remove('active');
-//     proyectSeven.classList.remove('active');
-//     proyectEight.classList.remove('active');
-// }
+const proyectTwo = document.querySelector('.proyect-2')
+const proyectButtonTwo = document.querySelector('.proyect-button_2');
+proyectButtonTwo.addEventListener('click', function(){
+    proyectOne.classList.remove('active');
+    proyectTwo.classList.add('active');
+    proyectThree.classList.remove('active');
+    proyectFour.classList.remove('active');
+    proyectFive.classList.remove('active');
+})
 
-// const proyectTwo = document.querySelector('.proyect-2');
-// proyectTwo.addEventListener('click', proyectTwoActive);
+const proyectThree = document.querySelector('.proyect-3');
+const proyectButtonThree = document.querySelector('.proyect-button_3');
+proyectButtonThree.addEventListener('click', function(){
+    proyectOne.classList.remove('active');
+    proyectTwo.classList.remove('active');
+    proyectThree.classList.add('active');
+    proyectFour.classList.remove('active');
+    proyectFive.classList.remove('active');
+})
 
-// function proyectTwoActive(){
-//     proyectOne.classList.remove('active');
-//     proyectTwo.classList.add('active');
-//     proyectThree.classList.remove('active');
-//     proyectFour.classList.remove('active');
-//     proyectFive.classList.remove('active');
-//     proyectSix.classList.remove('active');
-//     proyectSeven.classList.remove('active');
-//     proyectEight.classList.remove('active');
-// }
+const proyectFour = document.querySelector('.proyect-4');
+const proyectButtonFour = document.querySelector('.proyect-button_4');
+proyectButtonFour.addEventListener('click', function(){
+    proyectOne.classList.remove('active');
+    proyectTwo.classList.remove('active');
+    proyectThree.classList.remove('active');
+    proyectFour.classList.add('active');
+    proyectFive.classList.remove('active');
+})
 
-// const proyectThree = document.querySelector('.proyect-3');
-// proyectThree.addEventListener('click', proyectThreeActive);
+const proyectFive = document.querySelector('.proyect-5');
+const proyectButtonFive = document.querySelector('.proyect-button_5');
+proyectButtonFive.addEventListener('click', function(){
+    proyectOne.classList.remove('active');
+    proyectTwo.classList.remove('active');
+    proyectThree.classList.remove('active');
+    proyectFour.classList.remove('active');
+    proyectFive.classList.add('active');
+})
 
-// function proyectThreeActive(){
-//     proyectOne.classList.remove('active');
-//     proyectTwo.classList.remove('active');
-//     proyectThree.classList.add('active');
-//     proyectFour.classList.remove('active');
-//     proyectFive.classList.remove('active');
-//     proyectSix.classList.remove('active');
-//     proyectSeven.classList.remove('active');
-//     proyectEight.classList.remove('active');
-// }
+//Email
+const email = 'leosoplapuco29@gmail.com';
 
-// const proyectFour = document.querySelector('.proyect-4');
-// proyectFour.addEventListener('click', proyectFourActive);
-
-// function proyectFourActive(){
-//     proyectOne.classList.remove('active');
-//     proyectTwo.classList.remove('active');
-//     proyectThree.classList.remove('active');
-//     proyectFour.classList.add('active');
-//     proyectFive.classList.remove('active');
-//     proyectSix.classList.remove('active');
-//     proyectSeven.classList.remove('active');
-//     proyectEight.classList.remove('active');
-// }
-
-// const proyectFive = document.querySelector('.proyect-5');
-// proyectFive.addEventListener('click', proyectFiveActive);
-
-// function proyectFiveActive(){
-//     proyectOne.classList.remove('active');
-//     proyectTwo.classList.remove('active');
-//     proyectThree.classList.remove('active');
-//     proyectFour.classList.remove('active');
-//     proyectFive.classList.add('active');
-//     proyectSix.classList.remove('active');
-//     proyectSeven.classList.remove('active');
-//     proyectEight.classList.remove('active');
-// }
-
-// const proyectSix = document.querySelector('.proyect-6');
-// proyectSix.addEventListener('click', proyectSixActive);
-
-// function proyectSixActive(){
-//     proyectOne.classList.remove('active');
-//     proyectTwo.classList.remove('active');
-//     proyectThree.classList.remove('active');
-//     proyectFour.classList.remove('active');
-//     proyectFive.classList.remove('active');
-//     proyectSix.classList.add('active');
-//     proyectSeven.classList.remove('active');
-//     proyectEight.classList.remove('active');
-// }
-
-// const proyectSeven = document.querySelector('.proyect-7');
-// proyectSeven.addEventListener('click', proyectSevenActive);
-
-// function proyectSevenActive(){
-//     proyectOne.classList.remove('active');
-//     proyectTwo.classList.remove('active');
-//     proyectThree.classList.remove('active');
-//     proyectFour.classList.remove('active');
-//     proyectFive.classList.remove('active');
-//     proyectSix.classList.remove('active');
-//     proyectSeven.classList.add('active');
-//     proyectEight.classList.remove('active');
-// }
-
-// const proyectEight = document.querySelector('.proyect-8');
-// proyectEight.addEventListener('click', proyectEightActive);
-
-// function proyectEightActive(){
-//     proyectOne.classList.remove('active');
-//     proyectTwo.classList.remove('active');
-//     proyectThree.classList.remove('active');
-//     proyectFour.classList.remove('active');
-//     proyectFive.classList.remove('active');
-//     proyectSix.classList.remove('active');
-//     proyectSeven.classList.remove('active');
-//     proyectEight.classList.add('active');
-// }
-
-//Header animation
-// const header = document.querySelector('header');
-// let prevScrollPos = window.pageYOffset;
-
-//   window.onscroll = function() {
-//     let currentScrollPos = window.pageYOffset;
-
-//     if (prevScrollPos > currentScrollPos) {
-//         header.classList.remove('active')
-//     } else {
-//         header.classList.add('active')
-//     }
-//     prevScrollPos = currentScrollPos;
-// }
+const emailTag = document.querySelectorAll('.email');
+emailTag.forEach((emailTag) => {
+    emailTag.innerHTML = email;
+    navigator.clipboard.writeText(email);
+});
