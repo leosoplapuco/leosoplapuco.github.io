9806875828243448c4d0d40db814510de04c7f48
leosoplapuco <leosoplapuco29@gmail.com>
2024-04-13 17:28:15 -0500

    Actualización 6.9

--- Proyectos/GrúasEmemca/js/app.js
+++ Proyectos/GrúasEmemca/js/app.js
@@ -1,14 +1,18 @@
 //Header animation
 const header = document.querySelector('header');
+const whatsapp = document.querySelector('.whatsapp-container');
+
 window.addEventListener('scroll', Scroll);
 
 function Scroll(){
     var ypos=window.pageYOffset;
     if (ypos > 50){
         header.classList.add('active');
+        whatsapp.classList.add('active');
     }
     else{
         header.classList.remove('active');
+        whatsapp.classList.remove('active');
     }
 }
 
