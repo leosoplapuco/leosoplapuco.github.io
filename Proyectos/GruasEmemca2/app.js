da8fa5cb812f402ab271739358cd6fa8459fae91
leosoplapuco <leosoplapuco29@gmail.com>
2024-05-29 13:11:47 -0500

    Actualización 8.8

--- Proyectos/GruasEmemca2/app.js
+++ Proyectos/GruasEmemca2/app.js
@@ -27,3 +27,53 @@ function MenuDesactive(){
     Menu.classList.remove('active');
     Hamburguer.classList.remove('active');
 }
+
+const links = document.querySelectorAll('.link');
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        MenuDesactive();
+    });
+});
+
+//Machines
+fetch('app-data.json')
+.then(response => response.json())
+.then(data => {
+    const container = document.querySelector('.machines-container');
+    data.Maquinas.forEach(machine => {
+        const machineElement = document.createElement('a');
+        machineElement.href = `#Maquina-${machine.id}`;
+        machineElement.className = `machine machine-${machine.id}`;
+        machineElement.innerHTML = `
+            <div class="mahine-photo">
+                <img src="${machine['machine-img']}" alt="">
+            </div>
+            <h3 class="machine-name">${machine['machine-name']}</h3>
+            <p class="text machine-description">${machine['machine-description']}</p>
+        `;
+        container.appendChild(machineElement);
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
+
+//Recent works
+fetch('app-data.json')
+.then(response => response.json())
+.then(data => {
+    const WorksContainer = document.querySelector('.works');
+    data.Trabajos.forEach(work => {
+        const workElement = document.createElement('a');
+        workElement.href = `#Trabajo-${work.id}`;
+        workElement.className = `work work-${work.id}`;
+        workElement.innerHTML = `
+            <img src="${work['work-img']}" alt="">
+            <div class="works-info">
+                <h3 class="work-title">${work['work-title']}</h3>
+                <span class="work-date">${work['work-date']}</span>
+            </div>
+            <p class="text work-resume">${work['work-resume']}</p>
+        `;
+        WorksContainer.appendChild(workElement);
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
