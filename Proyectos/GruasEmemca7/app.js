39fcf7e8ecfbfc64e4808ff0654a0a3a12cc3478
leosoplapuco <leosoplapuco29@gmail.com>
2024-07-02 21:11:28 -0500

    Actualización 10.8

--- Proyectos/GruasEmemca7/app.js
+++ Proyectos/GruasEmemca7/app.js
@@ -33,3 +33,58 @@ function MenuActive(){
     MenuIcon.classList.toggle('active');
     Menu.classList.toggle('active');
 }
+
+//Maps
+const UbicationOne = document.querySelector('.ubication-1');
+const MapOne = document.querySelector('.map-1');
+UbicationOne.addEventListener('click', function(){
+    UbicationOne.classList.add('active');
+    MapOne.classList.add('active');
+
+    UbicationTwo.classList.remove('active');
+    MapTwo.classList.remove('active');
+})
+
+const UbicationTwo = document.querySelector('.ubication-2');
+const MapTwo = document.querySelector('.map-2');
+UbicationTwo.addEventListener('click', function(){
+    UbicationOne.classList.remove('active');
+    MapOne.classList.remove('active');
+
+    UbicationTwo.classList.add('active');
+    MapTwo.classList.add('active');
+})
+
+//Form
+
+const InputNumbers = document.querySelectorAll('.input-number');
+
+InputNumbers.forEach(InputNumber => {
+    InputNumber.addEventListener('input', (event) => {
+        if (event.target.value.length > 9) {
+            event.target.value = event.target.value.substring(0, 9);
+        }
+    });
+});
+
+const $form = document.querySelector('#contact-form');
+const FormSendTarget = document.querySelector('.form-contact-send');
+
+$form.addEventListener('submit', handleSubmit)
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form, headers:{'Accept': 'aplication/json'}
+    })
+    if (response.ok){
+        this.reset();
+        FormSend();
+    }
+}
+
+function FormSend() {
+    FormSendTarget.classList.add('active');
+    setTimeout(() => { FormSendTarget.classList.remove('active'); }, 5000);
+}
